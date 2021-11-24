1040974f90c7a1fc3f7a312f1d2336740c049b93
gauthama93 <52348293+gauthama93@users.noreply.github.com>
2021-11-24 17:23:12 +0530

    bugfix mks not connecting

--- octoprint_Julia2020AdvancedABLTouchUI/Main.py
+++ octoprint_Julia2020AdvancedABLTouchUI/Main.py
@@ -1877,7 +1877,10 @@ class ThreadSanityCheck(QtCore.QThread):
                     result = result.split('\n')  # each ssid and pass from an item in a list ([ssid pass,ssid paas])
                     result = [s.strip() for s in result]
                     for line in result:
-                        if 'FTDI' or 'ch341-uart' in line:
+                        if 'ch341-uart' in line:
+                            self.MKSPort = line[line.index('ttyUSB'):line.index('ttyUSB') + 7]
+                            print self.MKSPort
+                        elif 'FTDI' in line:
                             self.MKSPort = line[line.index('ttyUSB'):line.index('ttyUSB') + 7]
                             print self.MKSPort
 
